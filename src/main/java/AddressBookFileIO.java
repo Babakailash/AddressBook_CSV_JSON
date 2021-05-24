28b7e9a48d6b8b3ec377ab5a04ba83a110241ec8
Babakailash <nathkailash2020@gmail.com>
2021-05-24 17:04:34 +0530

    Return:Count CSV File In AddressBook Person Details

--- src/main/java/AddressBookFileIO.java
+++ src/main/java/AddressBookFileIO.java
@@ -1,4 +1,10 @@
 
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,6 +13,7 @@ import java.util.List;
 public class AddressBookFileIO {
 
     static  String FILE_NAME = "C:\\Users\\BABA\\AddressBook_CSV_JSON\\src\\main\\resources\\AddressBook_Data.txt";
+    public static String CSV_FILE_NAME = "C:\\Users\\BABA\\AddressBook_CSV_JSON\\src\\main\\resources\\AddressBook_CSV.csv";
     //DATA TEXT
     public void writeDataToFile(List<PersonDetails> personDetails) {
         StringBuffer buffer = new StringBuffer();
@@ -39,6 +46,40 @@ public class AddressBookFileIO {
             e.printStackTrace();
         }
         System.out.println("Count is  :" + count);
+        return count;
+    }
+    //CSV
+    public void writeDataToCSVfile(List<PersonDetails> personDetails) throws CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        try (
+                Writer writer = Files.newBufferedWriter(Paths.get(CSV_FILE_NAME));
+        ) {
+            StatefulBeanToCsv<PersonDetails> beanToCsv = new StatefulBeanToCsvBuilder(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .build();
+            beanToCsv.write(personDetails);
+        } catch ( IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void readCsvData() {
+        try {
+            Files.lines(new File(CSV_FILE_NAME).toPath())
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long CsvlistCount() {
+        long count = 0;
+        try {
+            count = Files.lines(new File(CSV_FILE_NAME).toPath())
+                    .count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Count is:" +count);
         return count;
     }
 }
--- src/test/java/AddressBookFileIOTest.java
+++ src/test/java/AddressBookFileIOTest.java
@@ -1,5 +1,9 @@
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 import java.util.Arrays;
 
 
@@ -17,4 +21,19 @@ public class AddressBookFileIOTest {
         long count = addressBookFileIO.listCount();
         Assertions.assertEquals(3, count);
     }
+
+    @Test
+    public void givenThreeAddressBookDataWhenWritten_tOCSV_FileShouldGiveResult() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        PersonDetails[] arrayOfData = {
+                new PersonDetails("Kailash", "Ayodhya", "Ayodhya", "UP", "42001", "9794445197", "baba@111"),
+                new PersonDetails("Moun10", "Meme", "New Ashok", "New Delhi", "42585", "993658568", "rip@567"),
+                new PersonDetails("King", "World", "Nagar", "New Delhi", "110096", "9205267464", "gip@789")
+        };
+        AddressBookFileIO addressBookFileIO = new AddressBookFileIO();
+        addressBookFileIO.writeDataToCSVfile(Arrays.asList(arrayOfData));
+        addressBookFileIO.readCsvData();
+        long count = addressBookFileIO.CsvlistCount();
+        Assertions.assertEquals(4,count);
+    }
+
 }
